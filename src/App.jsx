88284c45bee19ca6ecdc436834aa2d02a722da19
Krishan Dwivedi <krishandwive9@gmail.com>
2024-11-27 13:55:20 +0530

    added other utility classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Hero from "./components/Hero";
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-zentry underline text-violet-300">
-      Hello world!
-    </h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+    </main>
   );
 };
 
