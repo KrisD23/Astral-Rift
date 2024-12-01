a16db75b8a7837a21e2fbb686fe2645825664548
Krishan Dwivedi <krishandwive9@gmail.com>
2024-12-01 15:14:09 +0530

    added some texts

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -149,7 +149,7 @@ const Hero = () => {
               redefi<b>n</b>e
             </h1>
 
-            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100 font-bold">
               Enter the Metagame Layer <br /> Unleash the Play Economy
             </p>
 
